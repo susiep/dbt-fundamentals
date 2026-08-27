9848427dd6cd80d7c35ce645bd3e42695cb198c4
sphilp03@gmail.com <sphilp03@gmail.com>
2026-08-27 19:04:03 +0000

    testing staging model generation

---
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -0,0 +1,24 @@
+with 
+
+source as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+renamed as (
+
+    select
+        id,
+        orderid,
+        paymentmethod,
+        status,
+        amount,
+        created,
+        _batched_at
+
+    from source
+
+)
+
+select * from renamed
